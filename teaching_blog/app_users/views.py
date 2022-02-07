b6152ede1e7e9218c34d1455d6ef924177794029
Mbaje Rwaka Eric <erickmbanjerwaka@gmail.com>
2022-02-07 06:08:39 +0300

    last commit for to ngiht

--- teaching_blog/app_users/views.py
+++ teaching_blog/app_users/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from app_users.forms import UserForm, UserProfileInfoForm
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
-    return HttpResponse('this is my first page')
+    return render(request,'home.html')
 
     
 def register(request):
@@ -35,6 +38,30 @@ def register(request):
                              {'registered':registered,
                               'user_form':user_form,
                              'profile_form':profile_form})
+
+def user_login(request):
+    if request.method == "POST"
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirerct(reverse('index'))
+            else:
+                return HttpResponse("ACOUNT IS DEACTIVATED")
+         else:
+                return HttpResponse("please use the correct ID and password")
+
+    else:
+                return Render(request, 'app_users/login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
 
--- teaching_blog/teaching_blog/settings.py
+++ teaching_blog/teaching_blog/settings.py
@@ -10,11 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+from pathlib import path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'teaching_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
